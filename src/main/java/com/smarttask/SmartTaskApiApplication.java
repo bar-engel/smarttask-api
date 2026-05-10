1ac44d22b3b70f93d49163d294267ca669e49166
bar-engel <barengelwork@gmail.com>
2026-05-10 20:20:50 +0300

    feat: initialize Spring Boot 3 SmartTask API project structure

---
+++ src/main/java/com/smarttask/SmartTaskApiApplication.java
@@ -0,0 +1,12 @@
+package com.smarttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskApiApplication.class, args);
+    }
+}
